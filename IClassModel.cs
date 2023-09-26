d870b6f2ebbd0f88a4c2cbc684cc515135b93e83
bigfa <bigfa@LAPTOP-SS9VD3K4>
2023-09-26 11:57:07 -0400

    Add project files.

---
+++ IClassModel.cs
@@ -0,0 +1,22 @@
+﻿using QueryBuilderStarter;
+
+namespace QueryBuilderStarter
+{
+    public interface IClassModel
+    {
+        public int Id { get; set; }
+    }
+
+    public class Pokemon : IClassModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class BannedGame : IClassModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+
+}
